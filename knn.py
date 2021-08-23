018b11cc20ebf7498911eb99154ffbfed7b911f3
Liesy <preposterous@163.com>
2021-08-23 22:02:34 +0800

    8.23 dev-demo unfinished

---
+++ knn.py
@@ -0,0 +1,15 @@
+import numpy as np
+import time
+import torch
+
+
+class KNN(object):
+
+    def __init__(self, K):
+        self.K = K
+
+    def train(self, X, y):
+        pass
+
+    def predict(self, X, y):
+        pass
--- main.py
+++ main.py
@@ -6,16 +6,21 @@ import torch.nn.functional as F
 import numpy as np
 import pandas as pd
 
+from knn import *
+
 
 def main():
     '''程序入口'''
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--arg1', type=int, default=1)
-    parser.add_argument('--arg2', type=float, default=2.0)
+    parser.add_argument('--k', type=int, default=1)
 
     args = parser.parse_args()
 
+    knn_classifier = KNN(args.k)
+    
+
 
 if __name__ == '__main__':
+    
     main()
