8dd5ad6701cbe88aa6849ea79a2b0a3f77f3568c
Liesy <preposterous@163.com>
2021-08-24 10:58:56 +0800

    8.24 finish-knn

--- knn.py
+++ knn.py
@@ -38,7 +38,7 @@ class KNN(object):
         - y: A numpy array of shape (num_test,) containing predicted labels for the
           test data, where y[i] is the predicted label for the test point X[i].
         """
-
+        # https://mlxai.github.io/2017/01/03/finding-distances-between-data-points-with-numpy.html
         if num_loops == 0:
             dists = self.compute_distances_no_loops(X)
         elif num_loops == 1:
@@ -52,28 +52,65 @@ class KNN(object):
 
 
     def compute_distances_no_loops(self, X):
+        '''Compute the l2 distance between all test points and all training points'''
         num_test = X.shape[0]
         num_train = self.X_train.shape[0]
         dists = np.zeros((num_test, num_train))
+
+        M = np.dot(X, self.train_X.T) # M = X.dot(self.train_X.T) .T为转置
+        X_square = np.square(X).sum(axis=1)
+        train_X_square = np.square(self.train_X).sum(axis=1)
+
+        dists = np.sqrt(np.matrix(X_square).T + np.matrix(train_X_square) - 2 * M)
 
         return dists
 
 
     def compute_distances_one_loops(self, X):
+        '''Compute the l2 distance between the i_th test point and all training points'''
         num_test = X.shape[0]
         num_train = self.X_train.shape[0]
         dists = np.zeros((num_test, num_train))
+
+        for i in range(num_test):
+            dists[i, :] = np.sqrt(np.sum(np.square(X[i, :] - self.train_X), axis=1))
+
+        '''
+        np.sum(arr,axis)
+        当axis为0时,是压缩行,即将每一列的元素相加,将矩阵压缩为一行
+        当axis为1时,是压缩列,即将每一行的元素相加,将矩阵压缩为一列,以行向量的形式表示
+        '''
 
         return dists
 
 
     def compute_distances_two_loops(self, X):
+        '''Compute the l2 distance between the i_th test point and the j_th training point'''
         num_test = X.shape[0]
         num_train = self.X_train.shape[0]
         dists = np.zeros((num_test, num_train))
+
+        for i in range(num_test):
+            for j in range(num_train):
+                dists[i][j] = np.sqrt(np.sum(np.square(X[i, :] - self.train_X[j, :])))
 
         return dists
 
 
     def predict_labels(self, dists, k=1):
-        pass
+        '''
+        Use the distance matrix to find the k nearest neighbors of the i_th testing point
+        Use self.y_train to find the labels of these neighbors. 
+        Store these labels in closest_y. 
+        '''
+        num_test = dists.shape[0]
+        y_pred = np.zeros(num_test)
+
+        for i in range(num_test):
+            closest_y = []
+            '''argsort函数返回的是数组值从小到大的索引值'''
+            closest_y = self.y_train[np.argsort(dists[i])][0:k]
+            '''max返回closest_y.count(value)中最大的，即大多数投票'''
+            y_pred[i] = max(closest_y, key=list(closest_y).count)
+
+        return y_pred
